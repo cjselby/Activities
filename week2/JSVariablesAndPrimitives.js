a524029979bc66166c3f26c899e73609f4847040
Casey Selby <caseyjselby@gmail.com>
2022-04-12 18:47:47 -0700

    adding week 2.2 activities

---
+++ week2/JSVariablesAndPrimitives.js
@@ -0,0 +1,34 @@
+// create a constant variable (const)
+const tom = "Thumb"
+
+// create a variable that can be reassigned (let)
+let tree;
+
+// create three variables and assign them values of different data types (=)
+let aNum = 7e7;
+let story = "we can tell a story as part of this variables value";
+let theTruth = false;
+
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof aNum);
+console.log(typeof story);
+
+// change the value referenced by a variable (dynamic typing)
+story = 999;
+
+// print the type of the variable you just changed (typeof)
+console.log(typeof story);
+
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+
+let name = "John";
+let language = "Javascript";
+let number = '2';
+let time =  false;
+
+
+
+console.log("Hello, my name is " + name + ", and I am learning " + language + ". I have been practicing for only " +  number + " weeks, so my status as a master is: " + time + ".");
